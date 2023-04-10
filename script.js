201e48b5b0102bfd35626de8b68915f0badb2971
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-04-10 16:56:57 -0400

    Add grid size buttons and functionality

--- script.js
+++ script.js
@@ -5,32 +5,43 @@
     let base = document.querySelector(".baseContainer");
     let gridButton = document.querySelector(".newGrid");
     let resetButton = document.querySelector(".reset");
+    let bigGrid = document.querySelector(".bigGrid");
+    let medGrid = document.querySelector(".medGrid");
+    let smallGrid = document.querySelector(".smallGrid");
     resetButton.addEventListener("click", () => {newGrid(currentGrid)});
     gridButton.addEventListener("click", () => {newGrid(prompt("How many boxes per side?"))});
+    bigGrid.addEventListener("click", () => {currentSize = 960; newGrid(currentGrid)});
+    medGrid.addEventListener("click", () => {currentSize = 680; newGrid(currentGrid)});
+    smallGrid.addEventListener("click", () => {currentSize = 420; newGrid(currentGrid)});
     let currentGrid = 16;
+    let currentSize = 960;
 
     drawGrid(currentGrid);
 
     function hoverColor(e) {
         e.target.style["background"] = "red";
     }
-
-    function unHoverColor(e) {
-        e.target.style["background"] = "white";
-    }
     
     // Create drawGrid function
 
     function newGrid(dim) {
         base.removeChild(document.querySelector(".grid"));
+        if (dim > 100) {
+            alert("Maximum size is 100! Drawing 100x100 grid");
+            drawGrid(100);
+            currentGrid = 100;
+        } else {
         drawGrid(dim);
         currentGrid = dim;
+        }
     }
 
     function drawGrid(dim) {
 
         let grid = document.createElement('div');
         grid.classList.add("grid");
+        grid.style["width"] = `${currentSize}px`;
+        grid.style["height"] = `${currentSize}px`;
         base.appendChild(grid);
         
          // Create for loop for 16 div rows
