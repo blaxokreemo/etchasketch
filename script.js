961cf81705d83d3549efcd4945eed6323533d739
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-04-10 15:57:52 -0400

    Put grid logic into function drawGrid

--- script.js
+++ script.js
@@ -4,56 +4,68 @@
 
     let base = document.querySelector(".baseContainer");
 
-    // Create for loop for 16 div rows
+    drawGrid(16);
 
-    for (let row = 1; row <= 16; row++) {
-
-        // Create one div row
-
-        newRow = document.createElement('div');
-
-        // Add class of "row"
-
-        newRow.classList.add("row");
-
-        // Add class of "r[row number]"
-
-        newRow.classList.add(`r${row}`);
-
-        // Append div to baseContainer
-
-        base.appendChild(newRow);
-
-        // Create for loop for 16 div columns in each row
-
-        for (let column = 1; column <= 16; column++) {
-
-            // Create one div column
-
-            newCol = document.createElement('div');
-
-            // Add class of col
-
-            newCol.classList.add("col");
-
-            // Add class of "c[column number]"
-
-            newCol.classList.add(`c${column}`);
-
-            // Add unique grid ID
-
-            newCol.setAttribute("id", `${row}-${column}`);
-
-            // Append div to row
-
-            newRow.appendChild(newCol);
-
-        }
-
+    function hoverColor(e) {
+        e.target.style["background"] = "red";
     }
 
+    function unHoverColor(e) {
+        e.target.style["background"] = "white";
+    }
     
+    // Create drawGrid function
 
-    // Create for loop for 16 div columns in each row
+    function drawGrid(dim) {
+        
+         // Create for loop for 16 div rows
+        
+        for (let row = 1; row <= dim; row++) {
 
-    // Each row should be flex - set in styles.css
+            // Create one div row
+    
+            newRow = document.createElement('div');
+    
+            // Add class of "row"
+    
+            newRow.classList.add("row");
+    
+            // Add class of "r[row number]"
+    
+            newRow.classList.add(`r${row}`);
+    
+            // Append div to baseContainer
+    
+            base.appendChild(newRow);
+    
+            // Create for loop for 16 div columns in each row
+    
+            for (let column = 1; column <= dim; column++) {
+    
+                // Create one div column
+    
+                newCol = document.createElement('div');
+    
+                // Add class of col
+    
+                newCol.classList.add("col");
+    
+                // Add class of "c[column number]"
+    
+                newCol.classList.add(`c${column}`);
+    
+                // Add unique grid ID
+    
+                newCol.setAttribute("id", `${row}-${column}`);
+    
+                // Append div to row
+    
+                newCol.addEventListener('mouseover', hoverColor);
+                newCol.addEventListener('mouseout', unHoverColor);
+    
+                newRow.appendChild(newCol);
+    
+            }
+    
+        }
+    }
