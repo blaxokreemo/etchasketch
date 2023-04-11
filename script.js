72838159693015f5f121b179bd75ee08cc0c9f21
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-04-11 10:05:13 -0400

    Add color options bar and make the color of the pen changeable

--- script.js
+++ script.js
@@ -8,26 +8,40 @@
 
     let gridButton = document.querySelector(".newGrid");
     let resetButton = document.querySelector(".reset");
+
     let bigGrid = document.querySelector(".bigGrid");
     let medGrid = document.querySelector(".medGrid");
     let smallGrid = document.querySelector(".smallGrid");
+
+    let bluBtn = document.querySelector(".blue");
+    let ylwBtn = document.querySelector(".yellow");
+    let redBtn = document.querySelector(".red");
+    let blkBtn = document.querySelector(".black");
+    let fsyBtn = document.querySelector(".fantasy");
 
     const buttons = Array.from(document.querySelectorAll(".btn"));
     buttons.forEach(button => button.addEventListener("mouseover", btnOvr));
     buttons.forEach(button => button.addEventListener("mouseout", btnOut));
 
-    resetButton.addEventListener("click", () => {newGrid(currentGrid)});
-    gridButton.addEventListener("click", () => {newGrid(prompt("How many boxes per side?"))});
-    bigGrid.addEventListener("click", () => {currentSize = 900; newGrid(currentGrid)});
-    medGrid.addEventListener("click", () => {currentSize = 680; newGrid(currentGrid)});
-    smallGrid.addEventListener("click", () => {currentSize = 420; newGrid(currentGrid)});
+    resetButton.addEventListener("click", () => {newGrid(currentGrid, currentPen)});
+    gridButton.addEventListener("click", () => {newGrid(prompt("How many boxes per side?"), currentPen)});
+    bigGrid.addEventListener("click", () => {currentSize = 900; newGrid(currentGrid, currentPen)});
+    medGrid.addEventListener("click", () => {currentSize = 680; newGrid(currentGrid, currentPen)});
+    smallGrid.addEventListener("click", () => {currentSize = 420; newGrid(currentGrid, currentPen)});
+
+    const penButtons = Array.from(document.querySelectorAll(".pen"));
+    penButtons.forEach(button => button.addEventListener("click", changePen));
+    penButtons.forEach(button => button.addEventListener("click", () => {newGrid(currentGrid, currentPen)}));
     
     // STARTING GRID SIZE
 
     let currentGrid = 16;
     let currentSize = 680;
+    let currentHue = "0";
+    let currentSat = "0%";
+    let currentPen = hoverChoice;
 
-    drawGrid(currentGrid);
+    drawGrid(currentGrid, currentPen);
 
     // FUNCTIONS
 
@@ -43,12 +57,41 @@
 
     // COLORING
 
-    function hoverColor(e) {
-        if (e.target.getAttribute("data-lt") == "90") {
+    function changePen(e) {
+        if (e.target.classList.contains("fantasy")) {
+            currentPen = hoverFantasy;
+            return hoverFantasy;
+        } else {
+            currentPen = hoverChoice;
+            currentHue = e.target.getAttribute("data-hue");
+            currentSat = e.target.getAttribute("data-sat");
+            return hoverChoice;
+        }
+
+    }
+
+    function hoverChoice(e) {
+        if (e.target.getAttribute("data-lt") == "95") {
+            e.target.style["background"] = `hsl(${currentHue}, ${currentSat}, 95%)`;
+            e.target.setAttribute("data-lt", "90");
+        } else if (e.target.getAttribute("data-lt") =="50") {
+            return;
+        } else {
+            let lightness = e.target.getAttribute("data-lt");
+            e.target.style["background"] = `hsl(${currentHue}, ${currentSat}, ${lightness}%)`;
+            lightness = Number(lightness);
+            lightness = lightness - 5;
+            e.target.setAttribute("data-lt", `${lightness}`)
+        }
+
+    }
+
+    function hoverFantasy(e) {
+        if (e.target.getAttribute("data-lt") == "95") {
             let hue = randHue();
             let sat = randSat();
-            e.target.style["background"] = `hsl(${hue}, ${sat}%, 90%)`;
-            e.target.setAttribute("data-lt", "80");
+            e.target.style["background"] = `hsl(${hue}, ${sat}%, 95%)`;
+            e.target.setAttribute("data-lt", "90");
             e.target.setAttribute("data-hue", `${hue}`);
             e.target.setAttribute("data-sat", `${sat}`);
         } else if (e.target.getAttribute("data-lt") == "0") {
@@ -57,11 +100,12 @@
             let hue = e.target.getAttribute("data-hue");
             let sat = e.target.getAttribute("data-sat");
             let lightness = e.target.getAttribute("data-lt");
+
+            e.target.style["background"] = `hsl(${hue}, ${sat}%, ${lightness}%)`;
+
             lightness = Number(lightness);
             lightness = lightness - 10;
             e.target.setAttribute("data-lt", `${lightness}`)
-
-            e.target.style["background"] = `hsl(${hue}, ${sat}%, ${lightness}%)`;
         }
     }
 
@@ -75,19 +119,19 @@
     
     // GRID
 
-    function newGrid(dim) {
+    function newGrid(dim, pen) {
         base.removeChild(document.querySelector(".grid"));
         if (dim > 100) {
             alert("Maximum size is 100! Drawing 100x100 grid");
             drawGrid(100);
             currentGrid = 100;
         } else {
-        drawGrid(dim);
+        drawGrid(dim, pen);
         currentGrid = dim;
         }
     }
 
-    function drawGrid(dim) {
+    function drawGrid(dim, pen) {
 
         let grid = document.createElement('div');
         grid.classList.add("grid");
@@ -137,13 +181,13 @@
 
                 // Create color attributes
 
-                newCol.setAttribute("data-lt", "90")
+                newCol.setAttribute("data-lt", "95")
                 newCol.setAttribute("data-hue", "none")
                 newCol.setAttribute("data-sat", "none")
     
                 // Append div to row
     
-                newCol.addEventListener('mouseover', hoverColor);
+                newCol.addEventListener('mouseover', pen);
                 /* newCol.addEventListener('mouseout', unHoverColor);*/
     
                 newRow.appendChild(newCol);
